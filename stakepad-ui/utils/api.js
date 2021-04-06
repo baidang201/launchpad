a70e6c188bf8f6f235945c9acd6552da638c72a0
Li Smith <jinrishouji@sina.com>
2021-04-06 15:30:47 +0800

    1 rename to fetchProtobuf 2 add process.env.API_ENDPOINT env

--- stakepad-ui/utils/api.js
+++ stakepad-ui/utils/api.js
@@ -2,8 +2,8 @@ import protobuf from './protobuf.js'
 import fetch from 'node-fetch';
 import {logger} from './log.js'
 
-const URL = 'http://127.0.0.1:3000'
-export async function probufFetcher (url, data, resMessage) {
+const URL = process.env.API_ENDPOINT || 'http://127.0.0.1:3000'
+export async function fetchProtobuf(url, data, resMessage) {
   if (!data) {
     return
   }
@@ -41,54 +41,54 @@ export async function getWorkers(
       sort_field_name: sort_field_name,
       filter_stash_accounts: filter_stash_accounts}});
   const buffer = protobuf.CommonRequest.encode(message).finish();
-  probufFetcher(URL, buffer, protobuf.WorkerResponse);
+  fetchProtobuf(URL, buffer, protobuf.WorkerResponse);
 }
 
 export async function getGlobalStatistics() {
   const message = protobuf.CommonRequest.create({global_statistics_request: {}});
   const buffer = protobuf.CommonRequest.encode(message).finish();
 
-  return probufFetcher(URL, buffer, protobuf.GlobalStatisticsResponse)
+  return fetchProtobuf(URL, buffer, protobuf.GlobalStatisticsResponse)
 }
 
 export async function getAvgStakeHistory() {
   const message = protobuf.CommonRequest.create({avg_stake_request: {}});
   const buffer = protobuf.CommonRequest.encode(message).finish();
 
-  return probufFetcher(URL, buffer, protobuf.AvgStakeResponse)
+  return fetchProtobuf(URL, buffer, protobuf.AvgStakeResponse)
 }
 
 export async function getWorkerStakeHistory(stash_account) {
   const message = protobuf.CommonRequest.create({stake_info_request: {stash_account: stash_account}});
   const buffer = protobuf.CommonRequest.encode(message).finish();
 
-  return probufFetcher(URL, buffer, protobuf.StakeInfoResponse)
+  return fetchProtobuf(URL, buffer, protobuf.StakeInfoResponse)
 }
 
 export async function getAvgRewardHistory() {
   const message = protobuf.CommonRequest.create({avg_reward_request: {}});
   const buffer = protobuf.CommonRequest.encode(message).finish();
 
-  return probufFetcher(URL, buffer, protobuf.AvgStakeResponse)
+  return fetchProtobuf(URL, buffer, protobuf.AvgStakeResponse)
 }
 
 export async function getWorkerRewardHistory(stash_account) {
   const message = protobuf.CommonRequest.create({reward_penalty_request: {stash_account: stash_account}});
   const buffer = protobuf.CommonRequest.encode(message).finish();
 
-  return probufFetcher(URL, buffer, protobuf.RewardPenaltyResponse)
+  return fetchProtobuf(URL, buffer, protobuf.RewardPenaltyResponse)
 }
 
 export async function getApyHistory(stash_account) {
   const message = protobuf.CommonRequest.create({apy_request: {stash_account: stash_account}});
   const buffer = protobuf.CommonRequest.encode(message).finish();
 
-  return probufFetcher(URL, buffer, protobuf.ApyResponse)
+  return fetchProtobuf(URL, buffer, protobuf.ApyResponse)
 }
 
 export async function getCommissionHistory(stash_account) {
   const message = protobuf.CommonRequest.create({commission_request: {stash_account: stash_account}});
   const buffer = protobuf.CommonRequest.encode(message).finish();
 
-  return probufFetcher(URL, buffer, protobuf.CommissionResponse)
+  return fetchProtobuf(URL, buffer, protobuf.CommissionResponse)
 }
