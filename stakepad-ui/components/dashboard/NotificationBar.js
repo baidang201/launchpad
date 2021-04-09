b7609932eb3ec93afe295e39ece7308408543c02
amphineko <sukareki@gmail.com>
2021-04-10 04:46:40 +0800

    chore: reformat code

--- stakepad-ui/components/dashboard/NotificationBar.js
+++ stakepad-ui/components/dashboard/NotificationBar.js
@@ -1,13 +1,12 @@
-import { Button, Space, Row, Col } from 'antd';
-import { NotificationOutlined, CloseOutlined } from '@ant-design/icons';
-
+import { CloseOutlined, NotificationOutlined } from '@ant-design/icons'
+import { Button, Col, Row } from 'antd'
 
 function NotificationBar() {
-  return <Row style={{background: "black", color: "white"}} align="middle">
-      <Col span={1}><NotificationOutlined /></Col>
-      <Col span={22}>这是公告栏目</Col>
-      <Col span={1}> <Button type="text" style={{background: "black", color: "white"}}><CloseOutlined /></Button></Col>
-  </Row>
+    return <Row align="middle">
+        <Col span={1}><NotificationOutlined /></Col>
+        <Col span={22}>这是公告栏目</Col>
+        <Col span={1}> <Button type="text" style={{ background: 'black', color: 'white' }}><CloseOutlined /></Button></Col>
+    </Row>
 }
 
 export default NotificationBar
