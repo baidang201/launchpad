e75f9b881c7e398a1e6b211824a694b9e166c779
amphineko <sukareki@gmail.com>
2021-04-10 03:51:59 +0800

    re(dashboard/GlobalTelemetry): refactor

--- stakepad-ui/pages/dashboard.tsx
+++ stakepad-ui/pages/dashboard.tsx
@@ -1,17 +1,17 @@
-import { Col, Row, Space } from 'antd';
-import GlobalInfo from '../components/dashboard/GlobalInfo';
-import NotificationBar from '../components/dashboard/NotificationBar';
-import Search from '../components/dashboard/Search';
-import StakeInput from '../components/dashboard/StakeInput';
-import SwitchGroup from '../components/dashboard/SwitchGroup';
-import WorkerTable from '../components/dashboard/WorkerTable';
+import { Col, Row, Space } from 'antd'
+import { GlobalTelemetry } from '../components/dashboard/GlobalTelemetry'
+import NotificationBar from '../components/dashboard/NotificationBar'
+import Search from '../components/dashboard/Search'
+import StakeInput from '../components/dashboard/StakeInput'
+import SwitchGroup from '../components/dashboard/SwitchGroup'
+import WorkerTable from '../components/dashboard/WorkerTable'
 import styles from '../styles/pages/dashboard.module.css'
 
-function DashboardPage() {
+const DashboardPage: React.FC = () => {
     return (
         <Space className={styles.container} direction="vertical" size="large">
             <NotificationBar />
-            <GlobalInfo />
+            <GlobalTelemetry />
             <Row>
                 <Col span={12}><Search /></Col>
                 <Col span={12}><StakeInput /></Col>
