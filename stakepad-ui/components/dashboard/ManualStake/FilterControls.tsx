3b907e6aa054a8adb567dc27c858f83f81e59c10
amphineko <sukareki@gmail.com>
2021-04-11 23:14:24 +0800

    chore(dashboard/FilterControls): fix alignment

--- stakepad-ui/components/dashboard/ManualStake/FilterControls.tsx
+++ stakepad-ui/components/dashboard/ManualStake/FilterControls.tsx
@@ -1,15 +1,16 @@
 import Space from 'antd/lib/space'
 import Switch from 'antd/lib/switch'
+import styles from './FilterControls.module.css'
 
 export const FilterControls: React.FC = () => {
-    return <div>
+    return (
         <Space size="small">
-            <div><Switch defaultChecked /> 不超过 20% 分润</div>
-            <div><Switch defaultChecked /> 未满足基础抵押</div>
-            <div><Switch defaultChecked /> 运行</div>
-            <div><Switch defaultChecked /> 标记</div>
-        </Space>
-    </div>
+            <span className={styles.lineHeight2em}><Switch defaultChecked /> 不超过 20% 分润</span>
+            <span className={styles.lineHeight2em}><Switch defaultChecked /> 未满足基础抵押</span>
+            <span className={styles.lineHeight2em}><Switch defaultChecked /> 运行</span>
+            <span className={styles.lineHeight2em}><Switch defaultChecked /> 标记</span>
+        </Space >
+    )
 }
 
 export default FilterControls
