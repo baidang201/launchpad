e6517240bd6cf4624a6a39b3cec75c55db7cc303
Li Smith <jinrishouji@sina.com>
2021-04-15 21:43:19 +0800

    format the code

--- backend/buildProtoJson.js
+++ backend/buildProtoJson.js
@@ -1,7 +1,6 @@
-import pbjs from "protobufjs/cli/pbjs.js"
+import pbjs from 'protobufjs/cli/pbjs.js'
 
-pbjs.main([ "--target", "json", "../proto/message.proto" , "-o", "../proto/message.json"], function(err) {
-    if (err)
-        throw err;
+pbjs.main(['--target', 'json', '../proto/message.proto', '-o', '../proto/message.json'], function (err) {
+    if (err) { throw err }
     // do something with output
-});
+})
