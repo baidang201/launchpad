5da1199b2b5045665c36f4f0c2fc5d287608311b
amphineko <sukareki@gmail.com>
2021-05-03 06:17:45 +0800

    feat(deposit-panel): add error feedback

--- stakepad-ui/components/deposits/DepositPanel.tsx
+++ stakepad-ui/components/deposits/DepositPanel.tsx
@@ -7,6 +7,7 @@ import { Alert as AlertIcon, Check as CheckIcon } from 'baseui/icon'
 import { Input } from 'baseui/input'
 import { KIND as NotificationKind, Notification } from 'baseui/notification'
 import { ALIGN as RadioGroupAlign, Radio, RadioGroup } from 'baseui/radio'
+import { SIZE as SpinnerSize, StyledSpinnerNext } from 'baseui/spinner'
 import BN from 'bn.js'
 import React, { ReactElement, useMemo, useState } from 'react'
 import { deposit, withdraw } from '../../libs/extrinsics/deposit'
@@ -16,8 +17,8 @@ import { useAccountQuery } from '../../libs/queries/useAccountQuery'
 import { useDepositQuery } from '../../libs/queries/useBalanceQuery'
 import { AccountSelect } from '../accounts/AccountSelector'
 
-// const LoadingSpinner = (): ReactElement => <StyledSpinnerNext $as="span" $size={SpinnerSize.small} />
-const LoadingSpinner = (): ReactElement => <>Loading...</>
+const LoadingText = (): ReactElement => <>Loading...</>
+const LoadingSpinner = (): ReactElement => <StyledSpinnerNext $as="span" $size={SpinnerSize.small} />
 
 export const DepositPanel = ({ defaultAccount, defaultMode }: {
     defaultAccount?: AccountId
@@ -25,6 +26,7 @@ export const DepositPanel = ({ defaultAccount, defaultMode }: {
 }): ReactElement => {
     const [accountId, setAccountId] = useState<AccountId | undefined>(defaultAccount ?? undefined)
     const [amount, setAmount] = useState<number>()
+    const [lastError, setLastError] = useState<string>()
     const [mode, setMode] = useState<'deposit' | 'withdraw'>(defaultMode ?? 'deposit')
     const [extrinsicStatus, setExtrinsicStatus] = useState<ExtrinsicStatus>()
 
@@ -36,30 +38,34 @@ export const DepositPanel = ({ defaultAccount, defaultMode }: {
     const accountSelectCaption = useMemo(() => {
         switch (accountId !== undefined && mode) {
             case 'deposit':
-                return <>钱包余额: {accountInfo?.data.free.toHuman() ?? <LoadingSpinner />}</>
+                return <>钱包余额: {accountInfo?.data.free.toHuman() ?? <LoadingText />}</>
             case 'withdraw':
-                return <>挖矿储值: {depositBalance?.toHuman() ?? <LoadingSpinner />}</>
+                return <>挖矿储值: {depositBalance?.toHuman() ?? <LoadingText />}</>
             default:
                 return <>选择一个账户</>
         }
     }, [accountId, accountInfo?.data.free, depositBalance, mode])
 
-    const extrinsicStatusIndicator = useMemo(() => {
+    const extrinsicStatusNotification = useMemo(() => {
         switch (extrinsicStatus) {
             case undefined:
                 return <></>
             case 'localSign':
                 return <Notification kind={NotificationKind.warning}><LoadingSpinner /> 等待浏览器扩展签名</Notification>
             case 'broadcast':
-                return <Notification kind={NotificationKind.info}><LoadingSpinner /> Broadcasting</Notification>
+                return <Notification kind={NotificationKind.info}><LoadingSpinner /> 交易发送中</Notification>
             case 'inBlock':
-                return <Notification kind={NotificationKind.info}><LoadingSpinner /> In block</Notification>
+                return <Notification kind={NotificationKind.info}><LoadingSpinner /> 交易等待进入区块</Notification>
             case 'finalized':
-                return <Notification kind={NotificationKind.positive}><CheckIcon /> Finalized</Notification>
+                return <Notification kind={NotificationKind.positive}><CheckIcon /> 交易完成</Notification>
             default:
-                return <Notification kind={NotificationKind.negative}><AlertIcon /> Extrinsic 失败或异常</Notification>
+                return <Notification kind={NotificationKind.negative}><AlertIcon /> 交易失败或异常</Notification>
         }
     }, [extrinsicStatus])
+
+    const lastErrorNotification = useMemo(() => {
+        return lastError === undefined ? <></> : <Notification kind={NotificationKind.negative}><AlertIcon />{lastError}</Notification>
+    }, [lastError])
 
     const ready = useMemo(() => (
         accountId !== undefined &&
@@ -75,6 +81,7 @@ export const DepositPanel = ({ defaultAccount, defaultMode }: {
     const handleSubmit = (): void => {
         if (accountId === undefined || amount === undefined || api === undefined) { return }
 
+        setLastError(undefined);
         ({ deposit, withdraw })[mode]({
             account: encodeAddress(accountId),
             api,
@@ -83,7 +90,10 @@ export const DepositPanel = ({ defaultAccount, defaultMode }: {
                 setExtrinsicStatus(status)
             },
             value: new BN(amount * 1e4).mul(new BN(1e8)) as BalanceOf
-        }).catch(error => { throw error })
+        }).catch(error => {
+            setExtrinsicStatus('invalid')
+            setLastError((error as Error)?.message ?? error)
+        })
     }
 
     return (
@@ -110,7 +120,9 @@ export const DepositPanel = ({ defaultAccount, defaultMode }: {
 
             <Button disabled={!ready} onClick={() => handleSubmit()} startEnhancer={<CheckIcon />}>提交</Button>
 
-            {extrinsicStatusIndicator}
+            {lastErrorNotification}
+
+            {extrinsicStatusNotification}
 
             {apiReadystate !== 'ready' && <Notification kind={NotificationKind.negative}>Waiting for connection to Phala network</Notification>}
             {web3Readystate !== 'ready' && <Notification kind={NotificationKind.negative}>Waiting for Polkadot.js browser extension</Notification>}
