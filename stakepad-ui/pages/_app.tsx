9e0af634e0135536dda98cc8ddd3689b43caa2cf
amphineko <sukareki@gmail.com>
2021-05-22 16:51:32 +0800

    feat: add global loading indicator

--- stakepad-ui/pages/_app.tsx
+++ stakepad-ui/pages/_app.tsx
@@ -1,11 +1,14 @@
 import { Poc4 } from '@phala/typedefs'
 import { AppNavBar, NavItemT, setItemActive } from 'baseui/app-nav-bar'
 import { LayersManager } from 'baseui/layer'
+import { Spinner } from 'baseui/spinner'
+import { PLACEMENT as ToastPlacement, toaster, ToasterContainer } from 'baseui/toast'
 import { AppComponent, AppProps } from 'next/dist/next-server/lib/router/router'
 import { useRouter } from 'next/router'
-import { useMemo, useState } from 'react'
-import { QueryClient, QueryClientProvider } from 'react-query'
+import { Key, PropsWithChildren, ReactElement, useEffect, useMemo, useRef, useState } from 'react'
+import { QueryClient, QueryClientProvider, useIsFetching } from 'react-query'
 import { Provider as StyletronProvider } from 'styletron-react'
+import { v4 as uuidv4 } from 'uuid'
 import { endpoint as PhalaEndpoint } from '../libs/config'
 import { ApiPromiseProvider, AppName, Web3Provider } from '../libs/polkadot'
 import { createStyletron } from '../libs/styletron'
@@ -15,6 +18,38 @@ interface NavItemWithTarget extends NavItemT {
     info: {
         target: string
     }
+}
+
+const GlobalFetchingToasterContainerKey = uuidv4()
+
+const GlobalFetchingToasterContainer = ({ children }: PropsWithChildren<{}>): ReactElement => {
+    const isFetching = useIsFetching()
+
+    const loadingToaster = useRef<Key>()
+
+    useEffect(() => {
+        if (isFetching > 0 && loadingToaster.current === undefined) {
+            loadingToaster.current = toaster.info(
+                <Spinner />,
+                {
+                    closeable: false,
+                    key: GlobalFetchingToasterContainerKey,
+                    overrides: {
+                        Body: {
+                            style: { backgroundColor: 'black', width: 'auto' }
+                        }
+                    }
+                }
+            )
+        }
+
+        if (isFetching === 0 && loadingToaster.current !== undefined) {
+            toaster.clear(loadingToaster.current)
+            loadingToaster.current = undefined
+        }
+    }, [isFetching])
+
+    return (<ToasterContainer placement={ToastPlacement.bottomLeft}>{children}</ToasterContainer>)
 }
 
 const App: AppComponent = ({ Component, pageProps }: AppProps) => {
@@ -48,20 +83,22 @@ const App: AppComponent = ({ Component, pageProps }: AppProps) => {
                 <QueryClientProvider client={client}>
                     <StyletronProvider value={styletron}>
                         <LayersManager>
-                            <AppNavBar
-                                mainItems={mainItems}
-                                onMainItemSelect={item => handleMainItemSelect(item as NavItemWithTarget)}
-                                title="Stakepad"
-                            />
+                            <GlobalFetchingToasterContainer>
+                                <AppNavBar
+                                    mainItems={mainItems}
+                                    onMainItemSelect={item => handleMainItemSelect(item as NavItemWithTarget)}
+                                    title="Stakepad"
+                                />
 
-                            <div className={styles.container}>
-                                <Component {...pageProps} />
-                            </div>
+                                <div className={styles.container}>
+                                    <Component {...pageProps} />
+                                </div>
+                            </GlobalFetchingToasterContainer>
                         </LayersManager>
                     </StyletronProvider>
                 </QueryClientProvider>
             </Web3Provider>
-        </ApiPromiseProvider>
+        </ApiPromiseProvider >
     )
 }
 
